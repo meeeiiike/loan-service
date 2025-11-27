c40e2f5a99a88bc34ad648ab508f8e5dc525884d
meike <g00407095@atu.ie>
2025-11-27 08:23:33 +0000

    init commit version and deps added

---
+++ src/test/java/ie/atu/loan_service/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.loan_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
