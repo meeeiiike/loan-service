d00dca137008a4a8275b68b6534a86cce253b342
meike <g00407095@atu.ie>
2025-12-31 03:25:29 +0000

    loan now FINALLY communicates with book service :DDD

--- src/main/java/ie/atu/loan_service/service/LoanService.java
+++ src/main/java/ie/atu/loan_service/service/LoanService.java
@@ -1,6 +1,8 @@
 package ie.atu.loan_service.service;
 
+import ie.atu.loan_service.client.BookClient;
 import ie.atu.loan_service.client.NotificationClient;
+import ie.atu.loan_service.dto.BookDTO;
 import ie.atu.loan_service.errorHandling.DuplicateExceptionHandling;
 import ie.atu.loan_service.errorHandling.NotFoundException;
 import ie.atu.loan_service.client.UserClient;
@@ -19,11 +21,13 @@ public class LoanService {
     private final UserClient userClient;
     private final LoanRepository loanRepository;
     private final NotificationClient notificationClient;
+    private final BookClient bookClient;
 
-    public LoanService(UserClient userClient, LoanRepository loanRepository, NotificationClient notificationClient) {
+    public LoanService(UserClient userClient, LoanRepository loanRepository, NotificationClient notificationClient, BookClient bookClient) {
         this.userClient = userClient;
         this.loanRepository = loanRepository;
         this.notificationClient = notificationClient;
+        this.bookClient = bookClient;
     }
 
     // Defensive Copy of Loan List
@@ -40,11 +44,15 @@ public class LoanService {
 
     public Loan createLoan(Loan loan) {
         if(loanRepository.findByLoanId(loan.getLoanId()).isPresent()){
-            throw new DuplicateExceptionHandling(loan.getLoanId() + " Already Exists");
+            throw new DuplicateExceptionHandling(loan.getLoanId() + " Already Exist's");
         }
         UserDTO user = userClient.getUserById(loan.getUserId());
         if(user == null){
-            throw new NotFoundException(loan.getUserId() + " doesnt Exist");
+            throw new NotFoundException(loan.getUserId() + " Doesn't Exist");
+        }
+        BookDTO book = bookClient.getBookById(loan.getBookId());
+        if(book == null){
+            throw new NotFoundException(loan.getBookId() + " Doesn't Exist");
         }
         loan.setLoanDate(LocalDate.now());
         loan.setDueDate(LocalDate.now().plusWeeks(1));
